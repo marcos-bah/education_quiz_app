2323abf69c5420eaebeaa5e306d7438537e5409d
Marcos Bah <marcos.barbosa@unifei.edu.br>
2023-03-22 13:12:50 -0300

    add ajuste responsivo

--- lib/start/start_page.dart
+++ lib/start/start_page.dart
@@ -24,84 +24,92 @@ class _StartPageState extends State<StartPage> {
         elevation: 0,
         backgroundColor: Colors.transparent,
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Text("Regras", style: Theme.of(context).textTheme.headline3),
-          SizedBox(
-            height: 100,
-            width: width,
-          ),
-          Container(
-            width: widthReal,
-            padding: const EdgeInsets.all(20),
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(8),
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text("Regras", style: Theme.of(context).textTheme.headline3),
+            SizedBox(
+              height: 80,
+              width: width,
             ),
-            child: Column(
-              children: const [
-                RulesWidget(
-                  text: "Cores ",
-                  text2: "verdes",
-                  color: Color(0xff1F8654),
-                  description: "geralmente indicam ganho",
-                ),
-                Divider(),
-                RulesWidget(
-                  text: "Cores ",
-                  text2: "vermelhas",
-                  color: Color(0xffA40021),
-                  description: "geralmente indicam perdas",
-                ),
-                Divider(),
-                RulesWidget(
-                  text: "Os dados",
-                  color: Color(0xffA40021),
-                  description:
-                      "Possuem a mesma chance sempre, 1 em cada 6 (lados).",
-                ),
-                Divider(),
-                RulesWidget(
-                  text: "Respostas",
-                  color: Color(0xff191D63),
-                  description:
-                      "Quanto mais rápido responder, mais pontos ganha.",
-                ),
-              ],
+            Container(
+              width: widthReal,
+              padding: const EdgeInsets.all(20),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(8),
+              ),
+              child: Column(
+                children: const [
+                  RulesWidget(
+                    text: "Cores ",
+                    text2: "verdes",
+                    color: Color(0xff1F8654),
+                    description: "geralmente indicam ganho",
+                  ),
+                  Divider(),
+                  RulesWidget(
+                    text: "Cores ",
+                    text2: "vermelhas",
+                    color: Color(0xffA40021),
+                    description: "geralmente indicam perdas",
+                  ),
+                  Divider(),
+                  RulesWidget(
+                    text: "Os dados",
+                    color: Color(0xffA40021),
+                    description:
+                        "Possuem a mesma chance sempre, 1 em cada 6 (lados).",
+                  ),
+                  Divider(),
+                  RulesWidget(
+                    text: "Respostas",
+                    color: Color(0xff191D63),
+                    description:
+                        "Quanto mais rápido responder, mais pontos ganha.",
+                  ),
+                ],
+              ),
             ),
-          ),
-          SizedBox(
-            height: 120,
-            width: width,
-          ),
-          Consumer<QuizController>(builder: (context, quizController, child) {
-            return Consumer<UserModel>(builder: (context, user, child) {
-              return TextButton(
-                onPressed: () {
-                  if (quizController.quiz.isLastQuestion) {
-                    quizController.reset();
-                    user.reset();
-                  }
-                  Navigator.of(context).push(
-                    MaterialPageRoute(
-                      builder: (context) => const StartContextPage(),
-                    ),
-                  );
-                },
-                child: Text(
-                  !quizController.quiz.isLastQuestion ? "Continuar" : "Começar",
-                  style: Theme.of(context).textTheme.bodyText2!.copyWith(
-                        color: const Color(0xffF4F3F6),
-                        fontWeight: FontWeight.bold,
-                      ),
-                ),
-              );
-            });
-          })
-        ],
+          ],
+        ),
       ),
+      floatingActionButton: Padding(
+        padding: const EdgeInsets.only(bottom: 20),
+        child: Consumer<QuizController>(
+          builder: (context, quizController, child) {
+            return Consumer<UserModel>(
+              builder: (context, user, child) {
+                return TextButton(
+                  onPressed: () {
+                    if (quizController.quiz.isLastQuestion) {
+                      quizController.reset();
+                      user.reset();
+                    }
+                    Navigator.of(context).push(
+                      MaterialPageRoute(
+                        builder: (context) => const StartContextPage(),
+                      ),
+                    );
+                  },
+                  child: Text(
+                    !quizController.quiz.isLastQuestion
+                        ? "Continuar"
+                        : "Começar",
+                    style: Theme.of(context).textTheme.bodyText2!.copyWith(
+                          color: const Color(0xffF4F3F6),
+                          fontWeight: FontWeight.bold,
+                        ),
+                  ),
+                );
+              },
+            );
+          },
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
 }
--- lib/startContext/startContext_page.dart
+++ lib/startContext/startContext_page.dart
@@ -22,89 +22,177 @@ class _StartContextPageState extends State<StartContextPage> {
         elevation: 0,
         backgroundColor: Colors.transparent,
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Text("Contexto", style: Theme.of(context).textTheme.headline3),
-          SizedBox(
-            height: 100,
-            width: width,
-          ),
-          Container(
-            width: widthReal,
-            padding: const EdgeInsets.all(20),
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(8),
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text("Contexto", style: Theme.of(context).textTheme.headline3),
+            SizedBox(
+              height: 50,
+              width: width,
             ),
-            child: Text.rich(
-              TextSpan(
-                text: "Nesse jogo, você é ",
-                style: Theme.of(context).textTheme.bodyText2,
-                children: [
-                  TextSpan(
-                    text: "João",
-                    style: Theme.of(context).textTheme.bodyText2!.copyWith(
-                          color: const Color(0xff1F8C55),
-                        ),
-                  ),
-                ],
+            Container(
+              width: widthReal,
+              padding: const EdgeInsets.all(20),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(8),
               ),
-            ),
-          ),
-          SizedBox(
-            height: 20,
-            width: width,
-          ),
-          Container(
-            width: widthReal,
-            padding: const EdgeInsets.all(20),
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(8),
-            ),
-            child: Text.rich(
-              TextSpan(
-                text: "Um jovem que está no ensino fundamental. ",
-                style: Theme.of(context).textTheme.bodyText2,
-                children: [
-                  TextSpan(
-                    text: "Sem nenhum dinheiro",
-                    style: Theme.of(context).textTheme.bodyText2!.copyWith(
-                          color: const Color(0xffA40021),
-                        ),
-                  ),
-                ],
-              ),
-            ),
-          ),
-          SizedBox(
-            height: 120,
-            width: width,
-          ),
-          Consumer<QuizController>(builder: (context, quizController, child) {
-            return Consumer<UserModel>(builder: (context, user, child) {
-              return TextButton(
-                onPressed: () {
-                  Navigator.of(context).push(
-                    MaterialPageRoute(
-                      builder: (context) => const QuizPage(),
+              child: Text.rich(
+                TextSpan(
+                  text: "Nesse jogo, você é ",
+                  style: Theme.of(context).textTheme.bodyText2,
+                  children: [
+                    TextSpan(
+                      text: "João",
+                      style: Theme.of(context).textTheme.bodyText2!.copyWith(
+                            color: const Color(0xff1F8C55),
+                          ),
                     ),
-                  );
-                },
-                child: Text(
-                  !quizController.quiz.isLastQuestion ? "Continuar" : "Começar",
-                  style: Theme.of(context).textTheme.bodyText2!.copyWith(
-                        color: const Color(0xffF4F3F6),
-                        fontWeight: FontWeight.bold,
-                      ),
+                  ],
                 ),
-              );
-            });
-          })
-        ],
+              ),
+            ),
+            SizedBox(
+              height: 20,
+              width: width,
+            ),
+            Container(
+              width: widthReal,
+              padding: const EdgeInsets.all(20),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(8),
+              ),
+              child: Text.rich(
+                TextSpan(
+                  text: "Um jovem que está no ensino fundamental. ",
+                  style: Theme.of(context).textTheme.bodyText2,
+                  children: [
+                    TextSpan(
+                      text: "Sem nenhum dinheiro",
+                      style: Theme.of(context).textTheme.bodyText2!.copyWith(
+                            color: const Color(0xffA40021),
+                          ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 20,
+              width: width,
+            ),
+            Container(
+              width: widthReal,
+              padding: const EdgeInsets.all(20),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(8),
+              ),
+              child: Text.rich(
+                TextSpan(
+                  text:
+                      "Caro jogador, muito prazer, sou a professora e pesquisadora ",
+                  style: Theme.of(context).textTheme.bodyText2,
+                  children: [
+                    TextSpan(
+                      text: "Fernanda Barbosa Mateus. \n\n",
+                      style: Theme.of(context).textTheme.bodyText2!.copyWith(
+                            fontWeight: FontWeight.bold,
+                          ),
+                    ),
+                    TextSpan(
+                      text:
+                          "Este jogo foi desenvolvido como parte de um projeto de pesquisa no programa de ",
+                      style: Theme.of(context).textTheme.bodyText2!.copyWith(),
+                    ),
+                    TextSpan(
+                      text:
+                          "MESTRADO PROFISSIONAL EM GESTÃO, PLANEJAMENTO E ENSINO (PPG/GPE) ",
+                      style: Theme.of(context).textTheme.bodyText2!.copyWith(
+                            fontWeight: FontWeight.bold,
+                          ),
+                    ),
+                    TextSpan(
+                      text: "sob orientação do ",
+                      style: Theme.of(context).textTheme.bodyText2!.copyWith(),
+                    ),
+                    TextSpan(
+                      text: "Prof. Dr. Jesus Alexandre Tavares Monteiro",
+                      style: Theme.of(context).textTheme.bodyText2!.copyWith(
+                            fontWeight: FontWeight.bold,
+                          ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 20,
+              width: width,
+            ),
+            Container(
+              width: widthReal,
+              padding: const EdgeInsets.all(20),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(8),
+              ),
+              child: Text.rich(
+                TextSpan(
+                  text: "Código ISBN: ",
+                  style: Theme.of(context).textTheme.bodyText2,
+                  children: [
+                    TextSpan(
+                      text: "978-65-00-64696-2",
+                      style: Theme.of(context).textTheme.bodyText2!.copyWith(
+                            color: const Color(0xff1F8C55),
+                            fontWeight: FontWeight.bold,
+                          ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
+      floatingActionButton: Padding(
+        padding: const EdgeInsets.only(bottom: 20),
+        child: Consumer<QuizController>(
+          builder: (context, quizController, child) {
+            return Consumer<UserModel>(
+              builder: (context, user, child) {
+                return TextButton(
+                  onPressed: () {
+                    if (quizController.quiz.isLastQuestion) {
+                      quizController.reset();
+                      user.reset();
+                    }
+                    Navigator.of(context).push(
+                      MaterialPageRoute(
+                        builder: (context) => const QuizPage(),
+                      ),
+                    );
+                  },
+                  child: Text(
+                    !quizController.quiz.isLastQuestion
+                        ? "Continuar"
+                        : "Começar",
+                    style: Theme.of(context).textTheme.bodyText2!.copyWith(
+                          color: const Color(0xffF4F3F6),
+                          fontWeight: FontWeight.bold,
+                        ),
+                  ),
+                );
+              },
+            );
+          },
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
 }
